cc98fa486a95c4c27c6017f3babd698813335be7
poetries <jingguanliuye@gmail.com>
2016-12-31 22:40:32 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 $(function(){
+	var y = 0;
+    var _h = 0;
+    var _t = 0;
+    var newTop = 0;
+    var _height = 0;
+    var  percent = 0;
+    var scrollTextH = 0;
+	    
 	//menu show and hide
 	
 	$("#menu_btn").on("click",function(){
-		$("#menu_aside_content").toggle(1000);
+		$(".scrollCon").slideToggle(500);
 	});
 	$("#music_song_btn").on("click",function(){
 		$(".music_song_content").toggle(1000);
@@ -27,5 +35,41 @@ $(function(){
 		
 		$("body").css({"background":"url("+picUrl+")"+" no-repeat"});
 	});
+
 	
-})
+    //滚动条
+    $(".scrollBtn").mousedown(function (e) {
+
+        y = e.clientY; //鼠标按下距离浏览器顶部的距离
+        _h = $(".scrollBtn").offset().top; //滑块距离浏览器顶部的距离
+        _t = y-_h;//鼠标按下的位置距离滑块顶端的距离
+        _height =  $(".scrollBar").height()- $(".scrollBtn").height();
+
+        $(document).mousemove(function (e) {
+            newTop = e.clientY - _t - $(".scrollBar").offset().top;//鼠标按下的位置距离scrollBar顶部的距离
+            if (newTop<0){
+                newTop = 0;
+            }else if (newTop>_height){
+                newTop = _height;
+            }
+            $(".scrollBtn").css("top",newTop);
+            //document.title = newTop;
+
+            //计算滑块在能滑动范围内的比例
+            percent = newTop/_height;
+            scrollTextH = percent*($(".scrollText").height()-$(".scrollCon").height());
+            $(".scrollText").css("top",-scrollTextH);
+        });
+        $(document).mouseup(function () {
+            $(document).unbind("mousemove");
+            $(document).unbind("mouseup");
+
+        });
+        return false;//阻止浏览器默认行为
+    });
+	
+	//love 
+	$("#love").click(function(){
+		$(this).toggleClass("love");
+	})
+});
