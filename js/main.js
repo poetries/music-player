4f4c3db0e824ca5b3d138cb58a172cc2bbd51e1c
poetries <jingguanliuye@gmail.com>
2016-12-31 16:10:42 +0800

    update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+	//menu show and hide
+	
+	$("#menu_btn").on("click",function(){
+		$("#menu_aside_content").toggle(1000);
+	});
+	$("#music_song_btn").on("click",function(){
+		$(".music_song_content").toggle(1000);
+		
+	});
+	
+})
