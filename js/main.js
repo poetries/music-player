39fdee3a1c1572b37b61c00d7f74b8150f2d8044
poetries <jingguanliuye@gmail.com>
2017-01-01 09:12:56 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
+//H5音频文件的接口 
+window.AudioContext=window.AudioContext||
+					window.webkitAudioContext||
+					window.mozAudioContext;
+window.requestAnimationFrame = window.requestAnimationFrame ||
+							   window.webkitRequestAnimationFrame || 
+							   window.mozRequestAnimationFrame || 
+							   window.msRequestAnimationFrame;
+							   
 window.onload = function(){
+	var oBtnPlay = getId("play")
+	,   audio = getId("audio")
+	,	mark = true
+	;
+	
+	oBtnPlay.onclick = function(){
+		if(mark){
+			audio.play();
+		}else {
+			audio.pause();
+		}
+		mark = !mark;
+	}
 	
 }
 
+function getId(idName){
+	return document.getElementById(idName);
+}
