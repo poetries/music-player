b267ec3cf9173ee180c8413b8f5f01b2f7557daf
poetries1 <jingguanliuye3@163.com>
2017-02-19 08:01:43 +0800

    update

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require("gulp");
-var babel = require("gulp-babel");
-
-gulp.task("default", function () {
-  return gulp.src("src/*.js")// ES6 源码存放的路径
-    .pipe(babel()) 
-    .pipe(gulp.dest("js")); //转换成 ES5 存放的路径
-});
